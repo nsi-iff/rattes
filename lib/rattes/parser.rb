00cad9787d904a4fde69584c7dffcb9fb10719c6
Rodrigo Manhães <rmanhaes@gmail.com>
2014-06-10 12:50:40 -0300

    extracts repeated elements as arrays

--- lib/rattes/parser.rb
+++ lib/rattes/parser.rb
@@ -5,14 +5,29 @@ module Rattes
   class Parser
     class << self
       def parse(xml_file)
-        doc = Nokogiri::XML(xml_file)
+        doc = Nokogiri::XML(xml_file, nil, 'UTF-8')
         curriculum = doc.children.first
         result = parse_children(curriculum)
         result.extend(Methodize)
+        methodize_arrays(result)
         result
       end
 
       private
+
+      def methodize_arrays(hash)
+        hash.each do |key, value|
+          methodize_arrays(value) if value.is_a? Hash
+          if value.is_a? Array
+            value.
+              select {|e| e.is_a? Hash }.
+              each do |sub_hash|
+                sub_hash.extend(Methodize)
+                methodize_arrays(sub_hash)
+              end
+          end
+        end
+      end
 
       def parse_children(source, origin = {})
         collect_attributes(source, origin)
@@ -20,7 +35,13 @@ module Rattes
         result = origin
         elements.each do |element|
           name = normalize(element.name)
-          element_hash = result[name] = {}
+          if result[name].nil?
+            element_hash = result[name] = {}
+          else
+            result[name] = [result[name]]
+            element_hash  = {}
+            result[name] << element_hash
+          end
           collect_attributes(element, element_hash)
           collect_sub_elements(element, element_hash)
         end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -41,5 +41,47 @@ describe Rattes::Parser do
       outras = dados_gerais.outras_informacoes_relevantes
       expect(outras.outras_informacoes_relevantes).to eql 'alguma coisa'
     end
+
+    it 'extracts repeated elements as array' do
+      fat = dados_gerais.formacao_academica_titulacao
+      expect(fat.especializacao.count).to eq 2
+
+      esp1, esp2 = fat.especializacao
+      expect(esp1.sequencia_formacao).to eq "4"
+      expect(esp1.nivel).to eq "2"
+      expect(esp1.titulo_da_monografia).to eq "Um Framework para Resolver um Problema que Acabei de Criar"
+      expect(esp1.nome_do_orientador).to eq "Johnny Rotten"
+      expect(esp1.codigo_instituicao).to eq "123412341234"
+      expect(esp1.nome_instituicao).to eq "Instituto José das Couves"
+      expect(esp1.codigo_curso).to eq "12341234"
+      expect(esp1.nome_curso).to eq "Desenvolvimento de Software Orientado a Objetos"
+      expect(esp1.status_do_curso).to eq "CONCLUIDO"
+      expect(esp1.ano_de_inicio).to eq "2005"
+      expect(esp1.ano_de_conclusao).to eq "2006"
+      expect(esp1.flag_bolsa).to eq "SIM"
+      expect(esp1.codigo_agencia_financiadora).to eq "09876543"
+      expect(esp1.nome_agencia).to eq "FAPERJ"
+      expect(esp1.carga_horaria).to eq "360"
+      expect(esp1.titulo_da_monografia_ingles).to eq "An Awesome Unuseful Framework"
+      expect(esp1.nome_curso_ingles).to eq "Object-Oriented Software Development"
+
+      expect(esp2.sequencia_formacao).to eq "5"
+      expect(esp2.nivel).to eq "2"
+      expect(esp2.titulo_da_monografia).to eq "Um Sistema Bacana"
+      expect(esp2.nome_do_orientador).to eq "Sistemeiro"
+      expect(esp2.codigo_instituicao).to eq "123443211234"
+      expect(esp2.nome_instituicao).to eq "Escola Superior José das Couves"
+      expect(esp2.codigo_curso).to eq "9876543"
+      expect(esp2.nome_curso).to eq "Engenharia de Programas"
+      expect(esp2.status_do_curso).to eq "CONCLUIDO"
+      expect(esp2.ano_de_inicio).to eq "2010"
+      expect(esp2.ano_de_conclusao).to eq "2010"
+      expect(esp2.flag_bolsa).to eq "NAO"
+      expect(esp2.codigo_agencia_financiadora).to eq ""
+      expect(esp2.nome_agencia).to eq ""
+      expect(esp2.carga_horaria).to eq "480"
+      expect(esp2.titulo_da_monografia_ingles).to eq "A Cool System"
+      expect(esp2.nome_curso_ingles).to eq "Program Engineering"
+    end
   end
 end
